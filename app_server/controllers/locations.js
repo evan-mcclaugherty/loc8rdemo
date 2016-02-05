710f544fd37d204d77fbc062ad43bfdddbe1e2b4
evnmck <evnmck@yahoo.com>
2016-02-04 20:18:49 -0700

    homepage static layout with bootstrap

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,12 +1,12 @@
 module.exports.homelist = function(req,res) {
-  res.render('index', {
+  res.render('locations-list', {
     title: 'Home'
   });
 };
 module.exports.locationInfo = function(req, res) {
   res.render('index', {
     title: 'Location Info'
-  })
+  });
 };
 module.exports.addReview = function(req, res) {
   res.render('index', {
